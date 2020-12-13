2c51eeae835948b3af08be8dfb19cdaf2612fc29
Torleif Sandnes <tsandnes@akvagroup.com>
2020-12-13 15:11:04 +0100

    Phong shading works

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,21 @@ mod color;
 mod math;
 mod raytracer;
 mod util;
-use raytracer::Ray;
+use color::Color;
+use raytracer::{Material, PointLight, Ray};
 use raytracer::Sphere;
-use std::f64::consts;
+
 
 fn main() {
   let mut c = canvas::Canvas::new(100, 100);
-  let color = color::Color::new(1.0, 0.0, 0.0);
+  let mut color = color::Color::new(1.0, 0.0, 0.0);
 
   let mut sphere = Sphere::new();
-  sphere.transform = math::Matrix::shear(1., 0., 0., 0., 0., 0.) * math::Matrix::scale(0.5, 1., 1.);
+  sphere.material = Material::new();
+  sphere.material.color = Color::new(1., 0.2, 1.);
+  //sphere.transform = math::Matrix::shear(1., 0., 0., 0., 0., 0.) * math::Matrix::scale(0.5, 1., 1.);
+  let light = PointLight::new(&math::Tuple::point(-10., 10., -10.), &Color::new(1., 1., 1.));
+
   let ray_origin = math::Tuple::point(0., 0., -5.);
   let wall_size = 7.0;
   let pixel_size: f64 = wall_size / 100.;
@@ -26,6 +31,10 @@ fn main() {
       let r = Ray::new(&math::Tuple::point(0., 0., -5.), &ray_unit_vector);
       let xs = Ray::intersects(&sphere, &r);
       if xs.len() > 0 {
+        let point = Ray::position(&r, xs[0].t);
+        let normal = sphere.normal_at(&point);
+        let eye_vector = -r.direction;
+        let color = Material::lighting(&sphere.material, &light, &point, &eye_vector, &normal);
         c.write_pixel(x, y, color.clone());
       }
     }
